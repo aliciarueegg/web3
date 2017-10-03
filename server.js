173669132fd4c21905e6d89b01ed1631225e26bf
louisleon90 <louis.leon.90@gmail.com>
2017-10-03 10:42:21 +0200

    Initial Server setup (with errors)

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+"use strict";
+
 const
     http = require('http'),
-    server = http.createServer(function(req, res) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
+    fs = require('fs'),
+    url = require('url'),
+    path = require('path'),
+    index = fs.readFile('index.html'),
+    server = http.createServer(function(request, response) {
+        var uri = url.parse(request.url).pathname;
+        var filename = path.join(process.cwd(), uri);
+        console.log("serving: " + filename);
+        fs.readFile(filename, 'binary', function(err, file) {
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        })
     });
+
 server.listen(8080, function(){
     console.log('ready captain!');
 });
