8cbee4d32e91165d77db35e4b969ccd51dc9259c
louisleon90 <louis.leon.90@gmail.com>
2017-11-07 11:36:12 +0100

    Match the project and issue model to the API provided by our zen master

--- public/models/issue.js
+++ public/models/issue.js
@@ -1,13 +1,23 @@
 "use strict";
 
-function Issue(title, done, deleted, createDate) {
+function Issue(done,title,updated_at,project_client_id,priority,id,client_id,project_id,created_at) {
     this.done = done || false;
-    this.deleted = deleted || false;
-    this.title = title || "";
-    this.createDate = createDate;
+    this.title = title;
+    this.updated_at = udated_at || createDateAndTime();
+    this.project_client_id = project_client_id;
+    this.priority = priority || 0;
+    this.id = id || 0;
+    this.client_id = client_id;
+    this.project_id = project_id;
+    this.created_at = created_at || createDateAndTime();
 }
 
 Issue.prototype.add = function() {
 
 };
 
+function createDateAndTime (){
+    var date = new Date();
+    var month = date.getMonth()+1;
+    return date.getFullYear() + "-" + month + "-" + date.getDay() +  "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds() + "Z";
+}
