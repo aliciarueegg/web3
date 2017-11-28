18ec3b24f3df8126a43bf598a96c3521e1dc4ae2
Kaspar Wolfisberg <kaspar.wolfisberg@gmail.com>
2017-11-28 09:48:52 +0100

    Fix heroku port.

--- public/todo_server.js
+++ public/todo_server.js
@@ -3,11 +3,12 @@ const
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     app = express();
+		port = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + ''));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(8080, function(){
+app.listen(port, function(){
     console.log("ready captain.");
 });
