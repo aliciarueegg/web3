c9feebecd8782e41dfc796f80f5e3e91e1112f0f
Kaspar Wolfisberg <kaspar.wolfisberg@gmail.com>
2017-11-28 09:36:29 +0100

    Fix server file.

---
+++ public/todo_server.js
@@ -0,0 +1,13 @@
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + ''));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(80, function(){
+    console.log("ready captain.");
+});
